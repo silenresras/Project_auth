8339effe6639a1cf3074943bd8b2fbb3d84ac59a
RESRAS SILEN <mugafelhida98@gmail.com>
2025-11-26 17:28:14 -0800

    updated smtp configuration

--- emails/sendEmails.js
+++ emails/sendEmails.js
@@ -4,7 +4,7 @@ const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
-        secure: true, // true for 465, false for 587
+        secure: false, // true for 465, false for 587
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
